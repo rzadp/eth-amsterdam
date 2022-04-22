1abe3b671330be4f34dd6bd900e8eff3be20ac9a
rzadp <roopert7@gmail.com>
2022-04-22 23:38:49 +0200

    remove common

--- common/src/constants.ts
+++
@@ -1,1 +0,0 @@
-export const KovanMasterMarginEngine = '0x82ea0a9c578042154188d635d9c0e2e13ff6e846';
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
 import {
   useEtherBalance
 } from '@usedapp/core'
-import { KovanMasterMarginEngine } from 'eth-amsterdam-common'
 import { utils } from 'ethers'
 import React from 'react'
 
 export function App() {
-  const etherBalance = useEtherBalance(KovanMasterMarginEngine)
   return (
-    <div>MasterMarginEngine ETH balance: {etherBalance ? utils.formatEther(etherBalance) : 'Loading...'}</div>
+    <div>Hello World</div>
   )
 }
