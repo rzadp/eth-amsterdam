4143130f34908080c0a21f6c4f41cf461c4c19e5
rzadp <roopert7@gmail.com>
2022-04-23 00:15:28 +0200

    add useLiquidationFunction

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
-import {
-  useEtherBalance
-} from '@usedapp/core'
-import { utils } from 'ethers'
+import { useEthers } from '@usedapp/core'
 import React from 'react'
+import { useLiquidationFunction } from './useLiquidationFunction'
 
 export function App() {
-  return (
-    <div>Hello World</div>
+  const {send, state} = useLiquidationFunction()
+  const {account, activateBrowserWallet} = useEthers()
+
+  if (!account) return (
+    <button onClick={activateBrowserWallet}>Connect Wallet</button>
   )
+
+  return (<>
+    <div>Hello World</div>
+    <button onClick={() => {
+      send({
+        owner: '0x0000000000000000000000000000000000000000',
+        fixedLow: 1,
+        fixedHigh: 2
+      })
+    }}>Liquidate something</button>
+    {state.status !== 'None' && state.status}
+  </>)
 }
---
+++ frontend/src/useLiquidationFunction.ts
@@ -0,0 +1,29 @@
+import { utils } from 'ethers'
+import { Contract } from '@ethersproject/contracts'
+import MarginEngine from '../../voltz-core/deployments/kovan/MarginEngine.json'
+import { useContractFunction } from '@usedapp/core'
+
+const KovanMasterMarginEngine = '0x82ea0a9c578042154188d635d9c0e2e13ff6e846'
+const marginEngineInterface = new utils.Interface(MarginEngine.abi)
+const contract = new Contract(KovanMasterMarginEngine, marginEngineInterface)
+
+export interface LiquidatePositionArgs {
+  owner: string,
+  fixedLow: number,
+  fixedHigh: number
+}
+
+export const useLiquidationFunction = () => {
+  const { state, send } = useContractFunction(
+    contract,
+    'liquidatePosition',
+    { transactionName: 'liquidatePosition' }
+  )
+
+  return {
+    state,
+    send: ({owner, fixedLow, fixedHigh}: LiquidatePositionArgs) => {
+      send(owner, fixedLow, fixedHigh)
+    }
+  }
+}
