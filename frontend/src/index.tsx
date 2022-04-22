921ec5eeafe9e68b969ce2ad9250d0be2e6c8266
Tetiana Platonova <platonovatetiana@gmail.com>
2022-04-22 23:47:20 +0200

    Add apollo provider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,12 @@ import { Config, DAppProvider, Kovan } from '@usedapp/core'
 import { getDefaultProvider } from 'ethers'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client'
+
 import { App } from './App'
 
 const config: Config = {
@@ -11,9 +17,16 @@ const config: Config = {
   },
 }
 
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <DAppProvider config={config}>
-    <App />
-  </DAppProvider>,
+  <ApolloProvider client={client}>
+    <DAppProvider config={config}>
+      <App />
+    </DAppProvider>
+  </ApolloProvider>,
   document.getElementById('root')
 )
