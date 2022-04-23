7d8eb92273d24b84390ada88db48e15453f09ce3
rzadp <roopert7@gmail.com>
2022-04-23 12:03:08 +0200

    cosmetic

--- frontend/src/marginEngine.tsx
+++ frontend/src/marginEngine.tsx
@@ -3,8 +3,6 @@ import { utils } from 'ethers'
 import { Contract } from '@ethersproject/contracts'
 import MarginEngine from '../../voltz-core/deployments/kovan/MarginEngine.json'
 
-// const KovanMasterMarginEngine = '0xdcf2d0e379c29f67df42f6b720591ae66da48e3c'
-
 const marginEngineInterface = new utils.Interface(MarginEngine.abi)
 
 export const useMarginEngine = (marginEngineAddress: string | undefined): Contract | undefined => {
