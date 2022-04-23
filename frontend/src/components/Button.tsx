2c10be290105fc63f613befd100e6b869b1aa64d
Tetiana Platonova <platonovatetiana@gmail.com>
2022-04-23 22:20:45 +0200

    Add themed styles to button

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -2,24 +2,29 @@ import React from 'react'
 import styled from 'styled-components'
 import { COLORS } from 'src/styles/colors'
 
-export const Button = styled.button`
+export interface ButtonProps {
+  theme?: 'light' | 'dark'
+}
+
+export const Button = styled.button<ButtonProps>`
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
   padding: 6px 12px;
   border-radius: 6px;
-  border: none;
+  border: ${props => props.theme === 'light' ? `1px solid ${COLORS.primary400}` : 'none'};
   outline: none;
-  background-color: ${COLORS.primary500};
-  color: ${COLORS.white};
+  background-color: ${props => props.theme === 'light' ? COLORS.white : COLORS.primary500};
+  color: ${props => props.theme === 'light' ? COLORS.primary500 : COLORS.white};
+  font-weight: bold;
 
   &:hover {
-    background-color: ${COLORS.primary400};
+    background-color: ${props => props.theme === 'light' ? COLORS.primary100 :COLORS.primary400};
   }
   
   &:focus {
-    background-color: ${COLORS.primary500};
+    background-color: ${props => props.theme === 'light' ? COLORS.white :COLORS.primary500};
     box-shadow: 0px 2px 2px -1px rgba(0, 0, 0, 0.12), 0px 0px 0px 3px ${COLORS.shadow};
   }
 `
