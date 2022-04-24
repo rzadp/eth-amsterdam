bda58bb0a5beb85a35a1fa3f116ab65fc58ffae1
akoszowski <antoni.koszowski@trusttoken.com>
2022-04-24 03:02:10 +0200

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -2,9 +2,9 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { getPositions } from "./src/getPositions";
 import { PositionsStore } from "./src/PositionsStore";
-// import EpnsSDK from "@epnsproject/backend-sdk-staging";
-// import { sendNotification } from "./src/notifications";
-// import { CHANNEL_PK } from "./config";
+import EpnsSDK from "@epnsproject/backend-sdk-staging";
+import { sendNotification } from "./src/notifications";
+import { CHANNEL_PK } from "./config";
 
 const UPDATE_INTERVAL = 5 * 60 * 1000;
 
@@ -13,36 +13,32 @@ dotenv.config();
 const app: Express = express();
 const port = 8080;
 
-// const epnsSdk = new EpnsSDK(CHANNEL_PK);
+const epnsSdk = new EpnsSDK(CHANNEL_PK);
 
-const positionsStore = new PositionsStore();
+const positionsStore = new PositionsStore(console.log);
 
 app.get("/", async (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-// Dwa workery:
-// -> updater: aktualizuje wszystkie pozycje co 5min
-// -> closeWatcher: aktualizuje najbardziej zagrozone co 1min
+app.get("/epns/send-example", async (req: Request, res: Response) => {
+  const tx = await sendNotification(epnsSdk, {
+    recipientAddress: "0x222232c882677d524C4C1DD3AcD477ED7938F9d5",
+    notification: {
+      title: "Duck Title",
+      message: "Duck Message",
+    },
+    pushNotification: {
+      title: "Push Duck Title",
+      message: "Push DUck Message",
+    },
+  });
+  res.send(tx);
+});
 
-// app.get("/epns/send-example", async (req: Request, res: Response) => {
-//   const tx = await sendNotification(epnsSdk, {
-//     recipientAddress: "0x222232c882677d524C4C1DD3AcD477ED7938F9d5",
-//     notification: {
-//       title: "Duck Title",
-//       message: "Duck Message",
-//     },
-//     pushNotification: {
-//       title: "Push Duck Title",
-//       message: "Push DUck Message",
-//     },
-//   });
-//   res.send(tx);
-// });
-
-// app.get("/epns/get-users", async (req: Request, res: Response) => {
-//   res.send(await epnsSdk.getSubscribedUsers());
-// });
+app.get("/epns/get-users", async (req: Request, res: Response) => {
+  res.send(await epnsSdk.getSubscribedUsers());
+});
 
 app.get("/topWatched", async (req: Request, res: Response) => {
   res.send(positionsStore.getTopWatched());
