f163bd25e422a035538f79b17a0c8afb4e528063
akoszowski <antoni.koszowski@trusttoken.com>
2022-04-24 03:00:28 +0200

    Add positionsStore

--- backend/src/PositionsStore.ts
+++ backend/src/PositionsStore.ts
@@ -1,11 +1,13 @@
 import { PositionStats } from "types/Position";
 
+const NOTIFICATION_THRESHOLD = 50;
+
 export class PositionsStore {
   readonly WATCHED_NUMBER = 10;
   private topWatched: PositionStats[];
   private trackedPositions: { [id: string]: PositionStats };
 
-  constructor() {
+  constructor(private notify: (msg: string) => void) {
     this.topWatched = [];
     this.trackedPositions = {};
   }
@@ -15,20 +17,47 @@ export class PositionsStore {
   }
 
   addOrUpdatePosition(position: PositionStats) {
-    console.log(position);
     this.trackedPositions[position.id] = position;
 
-    this.updateTopWatched(position);
+    if (
+      this.topWatched.length < this.WATCHED_NUMBER ||
+      comparePositions(this.topWatched[this.topWatched.length - 1], position) >
+        0
+    ) {
+      this.updateTopWatched(position);
+    }
+
+    if (Object.keys(this.trackedPositions).length === NOTIFICATION_THRESHOLD) {
+      this.notify("INITIAL 10 top watched - NOTIFICATION");
+    }
   }
 
   private updateTopWatched(position: PositionStats) {
-    //   this.topWatched.filter
-    if (this.topWatched.length > this.WATCHED_NUMBER) {
+    const filtered = this.topWatched.filter(
+      (watched) =>
+        watched.id !== position.id || watched.marginAbs === position.marginAbs
+    );
+    if (
+      this.topWatched.some((watched) => watched.id === position.id) &&
+      filtered.length === this.topWatched.length
+    ) {
+      return;
+    }
+
+    this.topWatched = filtered;
+
+    if (this.topWatched.length === this.WATCHED_NUMBER) {
       this.topWatched.pop();
+      if (Object.keys(this.trackedPositions).length > NOTIFICATION_THRESHOLD) {
+        this.notify("Popping top watched - NOTIFICATION");
+      }
     }
 
     this.topWatched.push(position);
     this.topWatched.sort(comparePositions);
+    if (Object.keys(this.trackedPositions).length > NOTIFICATION_THRESHOLD) {
+      this.notify("New position in top watched - NOTIFICATION");
+    }
   }
 }
 
